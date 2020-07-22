97571d32a9b01b511f9f8b167524b348c7a0439e
김우수 <ws@WS-MacBookPro.local>
2020-07-22 12:30:02 +0900

    와이즈 인덱스 사이트 변경 반영 및 결과물 저장 폴더명 변경

--- 02_R_Quant_Portfolio_Investment/Chapter_05/02_WICE_기준_섹터정보_크롤링.R
+++ 02_R_Quant_Portfolio_Investment/Chapter_05/02_WICE_기준_섹터정보_크롤링.R
@@ -11,9 +11,10 @@ sec_tb <- list()
 for (i in sec_code_list) {
   url <- paste0(
     'http://www.wiseindex.com/Index/GetIndexComponets',
-    '?ceil_yn=0&dt=20190911&sec_cd=', i)
-  resp <- GET(url)
-  sec_tb[[i]] <- resp %>% content(as = 'text') %>% fromJSON() %>% .$list
+    '?ceil_yn=0&dt=20200721&sec_cd=', i)
+  data <- fromJSON(url)
+  data <- data$list
+  sec_tb[[i]] <- data
   Sys.sleep(1)
 }
 
@@ -21,9 +22,10 @@ sec_tb <- do.call(bind_rows, sec_tb) %>% as_tibble()
 sec_tb <- sec_tb %>% arrange(desc(MKT_VAL))
 
 files_path <- paste0(
-  '/03_R을_이용한_퀀트_투자_포트폴리오_만들기',
+  '/02_R_Quant_Portfolio_Investment',
   '/Chapter_05',
   '/files/')
+
 write.csv(
   sec_tb,
   file = paste0(getwd(), files_path, 'KOR_sector.csv'),
