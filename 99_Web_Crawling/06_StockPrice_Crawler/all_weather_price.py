05053c8bae7e1937b9e728ab3ce47f307192d6cb
wiskim <wiskimn@naver.com>
2025-01-22 07:34:13 -0800

    조회기간 변경

--- 99_Web_Crawling/06_StockPrice_Crawler/all_weather_price.py
+++ 99_Web_Crawling/06_StockPrice_Crawler/all_weather_price.py
@@ -70,7 +70,7 @@ resultDf = pd.DataFrame()
 
 for i in range(0, len(tickerList)):
     priceList = getYF(
-        tickerList[i], '2023-12-31', today
+        tickerList[i], '2024-12-31', today
     )
     priceList = priceList['AdjClose']
     resultDf[tickerList[i]] = priceList
